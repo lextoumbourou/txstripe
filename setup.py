b5f847165b7d79ad4b1bf9c5f977fade83696fe7
Lex Toumbourou <lextoumbourou@gmail.com>
2015-09-14 10:28:26 +1000

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='txstripe',
+    version='0.0.1',
+    description='Stripe Twisted bindings',
+    author='Lex Toumbourou',
+    author_email='lextoumbourou@gmail.com',
+    url='https://github.com/lextoumbourou/txstripe',
+    packages=['stripe', 'txstripe'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
